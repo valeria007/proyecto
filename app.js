14597af26df8fb7a30cfec0a16b3269c708e70ae
valeria007 <sis015abigail@gmail.com>
2018-05-24 15:12:17 -0400

    commit

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index');
+var service = require('./routes/api/v1.1/service');
+
 
 var app = express();
 
@@ -21,8 +22,9 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', index);
+app.use('/api/v1.1/ ', service);
+
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
